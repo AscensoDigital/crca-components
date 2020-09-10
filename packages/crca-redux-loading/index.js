ff631f7ea64591b3532f249a4fbd2d8badbc59e8
Claudio Corvalan <chiko6669@gmail.com>
2020-09-10 02:15:15 -0300

    se exponen en index funcionalidades para no tener que ir a cada archivo fuente

--- packages/crca-redux-loading/index.js
+++ packages/crca-redux-loading/index.js
@@ -1,1 +1,28 @@
 export { CrcaReduxLoading } from './src/CrcaReduxLoading.js';
+
+export { generateProcessTag } from './src/util/util';
+
+export {
+  startPageLoading,
+  startElementLoading,
+  startInternalLoading,
+  stopPageLoading,
+  stopElementLoading,
+  stopInternalLoading
+} from './src/redux/actions.js';
+
+export {
+  crcaLoadingPageSelector,
+  crcaLoadingElementSelector,
+  crcaLoadingInternalSelector,
+  crcaLoadingUntilElementSelector,
+  crcaLoadingSelector,
+  crcaLoadingPageExistProccessSelector,
+  crcaLoadingElementExistProccessSelector,
+  crcaLoadingInternalExistProccessSelector,
+  crcaLoadingExistProccessSelector
+} from './src/redux/selectors.js';
+
+export {
+ crcaLoading
+} from './src/redux/reducer.js';
