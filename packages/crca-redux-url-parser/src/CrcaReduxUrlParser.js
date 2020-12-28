c971aa0b20a2f4b3f15c5ced6b92ff889e72c596
Claudio Corvalan <chiko6669@gmail.com>
2020-12-28 13:23:38 -0300

    se implementa demo en webcomponenet

--- packages/crca-redux-url-parser/src/CrcaReduxUrlParser.js
+++ packages/crca-redux-url-parser/src/CrcaReduxUrlParser.js
@@ -1,6 +1,26 @@
 import { html, css, LitElement } from 'lit-element';
+import { connect } from 'pwa-helpers/connect-mixin';
+import { installRouter } from 'pwa-helpers/router';
+import { crcaStore } from '@ascenso/crca-redux-store';
+import { crcaUrl } from './redux/reducer.js';
+import {
+  crcaUrlAnchorSelector,
+  crcaUrlDominioSelector,
+  crcaUrlLastPageSelector,
+  crcaUrlPageSectionSelector,
+  crcaUrlPageSelector,
+  crcaUrlSearchSelector,
+  crcaUrlSubdominioSelector,
+  crcaUrlSectionParamsSelector
+} from './redux/selectors.js';
 
-export class CrcaReduxUrlParser extends LitElement {
+import { crcaUrlHandleNavigation } from './redux/actions.js';
+
+crcaStore.addReducers({
+  crcaUrl
+});
+
+export class CrcaReduxUrlParser extends connect(crcaStore)(LitElement) {
   static get styles() {
     return css`
       :host {
@@ -13,25 +33,60 @@ export class CrcaReduxUrlParser extends LitElement {
 
   static get properties() {
     return {
-      title: { type: String },
-      counter: { type: Number },
+      anchor: { type: String },
+      dominio: { type: String },
+      lastPage: { type: String },
+      page: { type: String },
+      pageSection: { type: String },
+      searchs: { type: Object },
+      sectionParams: { type: Array },
+      subdominio: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
+
+    installRouter((location, event) => {
+      // Only scroll to top on link clicks, not popstate events.
+      if (event && event.type === 'click') {
+        window.scrollTo(0, 0);
+      }
+      crcaStore.dispatch(crcaUrlHandleNavigation(location));
+    });
   }
 
-  __increment() {
-    this.counter += 1;
+  stateChanged(state) {
+    this.anchor = crcaUrlAnchorSelector(state);
+    this.dominio = crcaUrlDominioSelector(state);
+    this.lastPage = crcaUrlLastPageSelector(state);
+    this.page = crcaUrlPageSelector(state);
+    this.pageSection = crcaUrlPageSectionSelector(state);
+    this.searchs = crcaUrlSearchSelector(state);
+    this.sectionParams = crcaUrlSectionParamsSelector(state);
+    this.subdominio = crcaUrlSubdominioSelector(state);
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2>Datos Url Parseada</h2>
+      <ul>
+        <li><b>subdominio:</b> ${this.subdominio}</li>
+        <li><b>dominio:</b> ${this.dominio}</li>
+        <li><b>page:</b> ${this.page}</li>
+        <li><b>pageSection:</b> ${this.pageSection}</li>
+        <li><b>sectionParams:</b>
+          <ul>
+            ${this.sectionParams.map((sp, idx) => html`<li>${idx} => ${sp}</li>`)}
+          </ul>
+        </li>
+        <li><b>anchor:</b> ${this.anchor}</li>
+        <li><b>searchs:</b>
+          <ul>
+            ${Object.keys(this.searchs).map( idx => html`<li>${idx} => ${this.searchs[idx]}</li>`)}
+          </ul>
+        </li>
+      </ul>
     `;
   }
 }
