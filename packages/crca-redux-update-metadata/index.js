9394791daf3d9e72431c448ac70b3d4a602eed5f
Claudio Corvalan <chiko6669@gmail.com>
2021-01-21 13:18:53 -0300

    se expone action y selector

--- packages/crca-redux-update-metadata/index.js
+++ packages/crca-redux-update-metadata/index.js
@@ -1,1 +1,9 @@
 export { CrcaReduxUpdateMetadata } from './src/CrcaReduxUpdateMetadata.js';
+
+export {
+  updateMetadata
+} from './src/redux/actions.js';
+
+export {
+  crcaMetadataMetadataSelector
+} from './src/redux/selectors.js'
