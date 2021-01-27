3ff0e4d4406fb8527a336a7968588fc7243ee00b
Claudio Corvalan <chiko6669@gmail.com>
2021-01-27 00:08:56 -0300

    se pasan a archivo independiente constantes

---
+++ packages/crca-redux-firebase/src/consts.js
@@ -0,0 +1,18 @@
+export const FB_AUTH_EMAIL = 'email';
+export const FB_AUTH_TELEFONO = 'telefono';
+export const FB_AUTH_GOOGLE = 'google';
+export const FB_AUTH_PLAY_JUEGOS = 'playJuegos';
+export const FB_AUTH_GAME_CENTER = 'gameCenter';
+export const FB_AUTH_FACEBOOK = 'facebook';
+export const FB_AUTH_TWITTER = 'twitter';
+export const FB_AUTH_GITHUB = 'github';
+export const FB_AUTH_YAHOO = 'yahoo';
+export const FB_AUTH_MICROSOFT = 'microsoft';
+export const FB_AUTH_APPLE = 'apple';
+export const FB_AUTH_ANONYMOUSLY = 'anonymously';
+
+export const FB_RC_TYPE_VALUE_VALUE = 'value';
+export const FB_RC_TYPE_VALUE_BOOLEAN = 'boolean';
+export const FB_RC_TYPE_VALUE_NUMBER = 'number';
+export const FB_RC_TYPE_VALUE_STRING = 'string';
+export const FB_RC_TYPE_VALUE_OBJECT = 'object';
