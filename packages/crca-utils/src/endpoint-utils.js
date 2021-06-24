a1639c55ab6e440648f8b8468407f19de24fb663
Claudio Corvalan <chiko6669@gmail.com>
2021-06-24 16:25:54 -0400

    se elimina ? en caso de array vacio

--- packages/crca-utils/src/endpoint-utils.js
+++ packages/crca-utils/src/endpoint-utils.js
@@ -1,3 +1,3 @@
 import { isNull } from './data-utils.js';
 
-export const generateUrl = (urlParts, getVars = null) => `${urlParts.join('/')}${!isNull(getVars) ? `?${getVars.join('&')}` : ''}`;
+export const generateUrl = (urlParts, getVars = null) => `${urlParts.join('/')}${!isNull(getVars) && getVars.length ? `?${getVars.join('&')}` : ''}`;
