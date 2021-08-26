3b011d7dc9c3111bd852278411fa0aa1c5bf68a2
Claudio Corvalan <chiko6669@gmail.com>
2021-08-26 12:24:36 -0400

    se elimina packete para devtools y se configura manualmente

--- packages/crca-redux-store/src/crcaStore.js
+++ packages/crca-redux-store/src/crcaStore.js
@@ -1,13 +1,17 @@
 import {
   createStore,
   applyMiddleware,
-  combineReducers
+  combineReducers,
+  compose
 } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly.js';
 
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
 import thunk from 'redux-thunk';
+
+const composeWithDevTools =  process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  || compose
 
 export const crcaStore = createStore(
   state => state,
