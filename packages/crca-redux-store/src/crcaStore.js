9df88f834bee76218a068fa104fb9d6013b7b69a
Claudio Corvalan <chiko6669@gmail.com>
2021-08-25 23:28:29 -0400

    se elimina uso de dev_tools en produccion

--- packages/crca-redux-store/src/crcaStore.js
+++ packages/crca-redux-store/src/crcaStore.js
@@ -1,28 +1,17 @@
 import {
   createStore,
   applyMiddleware,
-  compose,
   combineReducers
 } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
 import thunk from 'redux-thunk';
 
-
-// Sets up a Chrome extension for time travel debugging.
-// See https://github.com/zalmoxisus/redux-devtools-extension for more information.
-const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-// const combinedReducers = combineReducers({
-//   app: app,
-//   counter: counter,
-// });
-
 export const crcaStore = createStore(
   state => state,
-  devCompose(
+  composeWithDevTools(
     applyMiddleware(thunk),
     lazyReducerEnhancer(combineReducers)
   )
