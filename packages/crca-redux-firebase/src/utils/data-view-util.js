052c49a49e38657a96e9800777c2d34a8128de84
Claudio Corvalan <chiko6669@gmail.com>
2022-09-19 21:33:44 -0300

    se usa firesotre/lite para obtener clase timestamp

--- packages/crca-redux-firebase/src/utils/data-view-util.js
+++ packages/crca-redux-firebase/src/utils/data-view-util.js
@@ -1,4 +1,4 @@
-import { Timestamp } from 'firebase/firestore';
+import { Timestamp } from 'firebase/firestore/lite';
 import { isDefined } from '@ascenso/crca-utils';
 
 export const dateFirebase2Js = date => {
