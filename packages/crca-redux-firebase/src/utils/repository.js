443752f3e28875e209e631ed24dd9996e2ebfa70
Claudio Corvalan <chiko6669@gmail.com>
2022-09-21 19:24:16 -0300

    se corrige if de verificacion de firestore

--- packages/crca-redux-firebase/src/utils/repository.js
+++ packages/crca-redux-firebase/src/utils/repository.js
@@ -1,13 +1,11 @@
 import { isNull } from '@ascenso/crca-utils';
-import { collection, query, where, orderBy } from "firebase/firestore";
+import { collection, query, where, orderBy, getFirestore } from "firebase/firestore";
 
 import { CrcaFirebaseLoader } from "../CrcaFirebaseLoader.js";
 
 export const getQueryCollectionByProp = (collectionName, prop, value) => {
-  if(!isNull(CrcaFirebaseLoader.db)) {
-    return false;
-  }
-  const collectionRef = collection(CrcaFirebaseLoader.db, collectionName);
+  const firestore =  isNull(CrcaFirebaseLoader.db) ? getFirestore() : CrcaFirebaseLoader.db;
+  const collectionRef = collection(firestore, collectionName);
   return query(collectionRef, where(prop, "==", value));
 };
 
@@ -18,11 +16,13 @@ export const getQueryCollectionByPropSorted = (
   sortProp = 'sort',
   sortOrder = 'asc'
 ) => {
-  const collectionRef = collection(CrcaFirebaseLoader.db, collectionName);
+  const firestore =  isNull(CrcaFirebaseLoader.db) ? getFirestore() : CrcaFirebaseLoader.db;
+  const collectionRef = collection(firestore, collectionName);
   return query(collectionRef, where(prop, "==", value), orderBy(sortProp, sortOrder));
 };
 
 export const getQueryCollectionSorted = (collectionName, sortProp = 'sort', sortOrder = 'asc') => {
-  const collectionRef = collection(CrcaFirebaseLoader.db, collectionName);
+  const firestore =  isNull(CrcaFirebaseLoader.db) ? getFirestore() : CrcaFirebaseLoader.db;
+  const collectionRef = collection(firestore, collectionName);
   return query(collectionRef, orderBy(sortProp, sortOrder));
 };
